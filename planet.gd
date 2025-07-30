156b37e0c496663e2bf2cdf9613319c070a5be42
carsonetb <blackhole243243@gmail.com>
2025-07-30 16:09:18 -0700

    Mister space ship

--- planet.gd
+++ planet.gd
@@ -3,15 +3,23 @@ extends AnimatableBody2D
 
 @export var orbit_enabled: bool = false
 @export var orbit_planet: Planet
-@export var orbit_speed: float = 5.0
+@export var orbit_speed: float = 0.01
 
+var distance: float = 0.0
+var current_angle: float = 0.0
+
+func _ready() -> void:
+	if !orbit_enabled:
+		return
+	
+	distance = global_position.distance_to(orbit_planet.global_position)
+	current_angle = orbit_planet.global_position.angle_to_point(global_position)
 
 func _physics_process(delta: float) -> void:
 	if !orbit_enabled:
 		return
 	
-	var angle: float = global_position.angle_to_point(orbit_planet.global_position)
+	current_angle += orbit_speed * delta * 60
 	
-	constant_linear_velocity = Vector2.from_angle(angle + PI/2) * orbit_speed
-	
-	global_position += constant_linear_velocity * delta * 60
+	constant_linear_velocity = (orbit_planet.global_position + Vector2.from_angle(current_angle) * distance) - global_position
+	global_position = orbit_planet.global_position + Vector2.from_angle(current_angle) * distance
--- player.gd
+++ player.gd
@@ -1,7 +1,9 @@
 class_name Player
 extends RigidBody2D
 
-const JUMP_VEL: float = 1000
+const JUMP_VEL: float = 600
+const LONG_JUMP_VEL: float = 1000
+const BOOSTER_VEL: float = 700
 const ACCEL: float = 300
 
 @onready var just_jumped_timer: SceneTreeTimer = get_tree().create_timer(0)
@@ -11,24 +13,33 @@ const ACCEL: float = 300
 
 var jump_velocity: Vector2
 var strafe_velocity: Vector2
+var closest_area: GravityArea
 
 func _physics_process(delta: float) -> void:
 	var areas = gravity_detection_area.get_overlapping_areas()
 	var up_direction: Vector2
 	
-	print(just_jumped_timer.time_left)
+	var closest_distance: float = INF
 	
+	var has_gravity_area = false
 	for area in areas:
 		if area is GravityArea:
+			has_gravity_area = true
 			area = area as GravityArea
 			rotation = (global_position - area.global_position).angle() + PI / 2.0
 			jump_velocity += (area.global_position - global_position).normalized() * area.accel
 			up_direction = (global_position - area.global_position).normalized()
+			
+			if area.position.distance_to(position) < closest_distance:
+				closest_area = area
 	
 	if _is_on_ground() && just_jumped_timer.time_left == 0:
 		jump_velocity = Vector2.ZERO
 	
-	if Input.is_action_just_pressed("jump") && _is_on_ground():
+	if Input.is_action_just_pressed("long_jump") && _is_on_ground():
+		just_jumped_timer = get_tree().create_timer(0.1)
+		jump_velocity = up_direction * LONG_JUMP_VEL
+	elif Input.is_action_just_pressed("jump") && _is_on_ground():
 		just_jumped_timer = get_tree().create_timer(0.1)
 		jump_velocity = up_direction * JUMP_VEL
 	
@@ -44,10 +55,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		strafe_velocity = Vector2(0,0)
 	
+	if !has_gravity_area:
+		rotation -= 3 * direction * delta
+		if Input.is_action_pressed("jump"):
+			jump_velocity = Vector2.from_angle(rotation-PI/2) * BOOSTER_VEL
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	print(jump_velocity, " ", strafe_velocity)
-	linear_velocity = jump_velocity + strafe_velocity
+	var planet_velocity: Vector2 = Vector2.ZERO
+	if closest_area:
+		planet_velocity = (closest_area.get_parent() as Planet).constant_linear_velocity * 60
+	linear_velocity = jump_velocity + strafe_velocity + planet_velocity
 
 func _is_on_ground() -> bool:
 	return ground_raycast.is_colliding()
