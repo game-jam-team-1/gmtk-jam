de86d8785b5d55cd3f1c1d149469a30f72770e9e
carsonetb <blackhole243243@gmail.com>
2025-07-31 14:00:10 -0700

    Minimap

---
+++ Scripts/minimap.gd
@@ -0,0 +1,31 @@
+class_name Minimap
+extends Node2D
+
+const scale_factor: float = 30.0
+
+@onready var world: World = get_parent().get_parent()
+@export var player: Player
+
+func _process(delta: float) -> void:
+	queue_redraw()
+
+func _convert_to_local(pos: Vector2):
+	var converted: Vector2 = (pos - player.global_position) / scale_factor
+	if converted.x > 60 || converted.x < -60 || converted.y > 60 || converted.y < -60:
+		return Vector2.INF
+	return converted
+
+func _get_alpha(local_pos: Vector2) -> float:
+	return 1 - local_pos.length() / 60
+
+func _draw() -> void:
+	var planets: Array[Planet]
+	
+	for node in world.get_children():
+		if node is Planet:
+			planets.append(node)
+	
+	for planet in planets:
+		var local: Vector2 = _convert_to_local(planet.global_position)
+		draw_circle(local, planet.radius / scale_factor, Color(1, 0.5, 0.3, _get_alpha(local)))
+	draw_circle(Vector2.ZERO, 3.0, Color.WHITE)
