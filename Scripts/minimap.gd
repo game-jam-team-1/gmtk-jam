9aa4e33f430254583ed4e71cf90f0a051af8ead6
carsonetb <blackhole243243@gmail.com>
2025-07-31 14:50:14 -0700

    Dialog box

--- Scripts/minimap.gd
+++ Scripts/minimap.gd
@@ -1,7 +1,7 @@
 class_name Minimap
 extends Node2D
 
-const scale_factor: float = 30.0
+const scale_factor: float = 50.0
 
 @onready var world: World = get_parent().get_parent()
 @export var player: Player
@@ -20,12 +20,18 @@ func _get_alpha(local_pos: Vector2) -> float:
 
 func _draw() -> void:
 	var planets: Array[Planet]
+	var enemies: Array[Node2D]
 	
 	for node in world.get_children():
 		if node is Planet:
 			planets.append(node)
+		if node is CrawlerEnemy:
+			enemies.append(node)
 	
 	for planet in planets:
 		var local: Vector2 = _convert_to_local(planet.global_position)
 		draw_circle(local, planet.radius / scale_factor, Color(1, 0.5, 0.3, _get_alpha(local)))
+	for enemy in enemies:
+		var local: Vector2 = _convert_to_local(enemy.global_position)
+		draw_circle(local, 2.0, Color(1, 0, 0, _get_alpha(local)))
 	draw_circle(Vector2.ZERO, 3.0, Color.WHITE)
