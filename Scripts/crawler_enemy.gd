83bacc35a855f9aa6e59e0d4e85c3910ba3b260f
Xavier Jin <xavierbunbunny@gmail.com>
2025-07-30 22:21:13 -0700

    Crawly guy is here, all is well and good in the world

---
+++ Scripts/crawler_enemy.gd
@@ -0,0 +1,72 @@
+class_name CrawlerEnemy
+extends RigidBody2D
+
+const CRAWL_VEL = 300
+
+@export var clockwise: bool = true
+
+var direction: int
+var crawl_velocity: Vector2
+var attraction_velocity: Vector2 = Vector2.ZERO
+var closest_gravity_area = null
+
+@onready var gravity_detection_area = $"GravityDetection"
+@onready var ground_raycast = $"GroundRaycast"
+
+func _ready() -> void:
+	if clockwise:
+		direction = 1
+	else:
+		direction = -1
+		#scale flipping doesnt really work rn
+		scale.x = -scale.x
+
+func _physics_process(delta: float) -> void:
+	var up_direction: Vector2 = Vector2.ZERO
+	
+	_process_gravity_area()
+	
+	if closest_gravity_area != null:
+		rotation = (global_position - closest_gravity_area.global_position).angle() + PI / 2
+		up_direction = (global_position - closest_gravity_area.global_position).normalized()
+		attraction_velocity += (closest_gravity_area.global_position - global_position).normalized() * closest_gravity_area.accel
+	
+	if is_on_ground():
+		attraction_velocity = Vector2.ZERO
+		crawl_velocity = up_direction.rotated(90 * direction) * CRAWL_VEL
+	else:
+		crawl_velocity = Vector2.ZERO
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var planet_velocity: Vector2 = Vector2.ZERO
+	if closest_gravity_area:
+		planet_velocity = (closest_gravity_area.get_parent() as Planet).constant_linear_velocity * 60
+	linear_velocity = crawl_velocity + planet_velocity + attraction_velocity
+
+
+func _process_gravity_area() -> void:
+	var areas = gravity_detection_area.get_overlapping_areas()
+	
+	if areas.size() == 0:
+		closest_gravity_area = null
+	
+	var current_gravity_dist: float = 9999999
+	if closest_gravity_area != null:
+		current_gravity_dist = global_position.distance_to(closest_gravity_area.global_position)
+	
+	for area in areas:
+		if area is not GravityArea:
+			return
+		
+		area = area as GravityArea
+		
+		if area.global_position.distance_to(global_position) < current_gravity_dist:
+			closest_gravity_area = area
+
+func is_on_ground() -> bool:
+	return ground_raycast.is_colliding()
+
+func flip():
+	clockwise = !clockwise
+	direction = -direction
+	scale.x = -scale.x
