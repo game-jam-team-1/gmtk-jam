95611363fcccf4c3f543a23c98032764e74403cd
carsonetb <blackhole243243@gmail.com>
2025-08-01 13:20:39 -0700

    Redesigned main level

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,7 @@
 class_name MainMenu
 extends Node2D
 
-@onready var level = preload("uid://debprahfmmn1a")
+@onready var level = preload("res://Scenes/world.tscn")
 
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_packed(level)
